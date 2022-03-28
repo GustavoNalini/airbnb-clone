05b607a9c25f06a04d4eb6331657b4349929af6f
Gustavo Nalini <gustavonalini@Gustavos-MacBook-Air.local>
2022-03-28 16:53:47 -0300

    debug /'res.json'

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home( { exploreData } ) {
 export async function getStaticProps() {
   const exploreData = await fetch ('https://links.papareact.com/pyp'). 
   then(
-    (res) => res.jason()
+    (res) => res.json()
   );
 
   return {
